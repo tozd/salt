2bfc06badb4713522b743474ef9e965a3be5a5a1
Mitar <mitar.git@tnode.com>
2018-10-17 10:38:59 -0700

    Fixing workaround.

--- docker/log-rotate-workaround.sh
+++ docker/log-rotate-workaround.sh
@@ -6,7 +6,7 @@ export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:$PATH"
 #  svlogd: pausing: unable to rename current: /var/log/mongod: access denied
 # This script tries to find such programs and restarts their containers.
 
-for PID in $(ps -o pid,cmd -A | grep 'svlogd: pausing' | cut -d ' ' -f 1) ; do
+for PID in $(ps -o pid,cmd -A | grep 'svlogd: pausing' | awk '{print $1}') ; do
   CONTAINER=$(container-from-pid "$PID" 2>/dev/null || true)
 
   if [[ -z "$CONTAINER" ]]; then
