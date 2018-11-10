b6564ded00e20fec09a3e0a510650557158b509e
Mitar <mitar.git@tnode.com>
2018-11-09 20:34:04 -0800

    Better version of the script.

--- docker/container-from-pid.sh
+++ docker/container-from-pid.sh
@@ -1,37 +1,4 @@
 #!/bin/bash -e
 # Prints the name of the container inside which the process with a PID on the host is.
 
-function getName {
-  local pid="$1"
-
-  if [[ -z "$pid" ]]; then
-    >&2 echo "Missing host PID argument."
-    exit 1
-  fi
-
-  if [ "$pid" -eq "1" ]; then
-    >&2 echo "Unable to resolve host PID to a container name."
-    exit 2
-  fi
-
-  # ps returns values potentially padded with spaces, so we pass them as they are without quoting.
-  local parentPid="$(ps -o ppid= -p $pid)"
-  if [[ -z "$parentPid" ]]; then
-    >&2 echo "Unable to resolve host PID to a container name."
-    exit 2
-  fi
-  local containerId="$(ps -o args= -f -p $parentPid | grep docker-containerd-shim | cut -d ' ' -f 2)"
-
-  if [[ -n "$containerId" ]]; then
-    local containerName="$(docker inspect --format '{{.Name}}' "$containerId" | sed 's/^\///')"
-    if [[ -n "$containerName" ]]; then
-      echo "$containerName"
-    else
-      echo "$containerId"
-    fi
-  else
-    getName "$parentPid"
-  fi
-}
-
-getName "$1"
+docker inspect --format '{{.Name}}' "$(cat /proc/$1/cgroup | head -n 1 | cut -d / -f 3)" | sed 's/^\///'
