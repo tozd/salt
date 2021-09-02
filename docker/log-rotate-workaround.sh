7833579d88e591fe4b8a0a732c6c9efe767f853a
Mitar <mitar.git@tnode.com>
2021-09-02 23:02:53 +0200

    Also look in docker logs.

--- docker/log-rotate-workaround.sh
+++ docker/log-rotate-workaround.sh
@@ -4,6 +4,8 @@ export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:$PATH"
 
 # Programs in Docker containers sometimes hang with an error like:
 #  svlogd: pausing: unable to rename current: /var/log/mongod: access denied
+# Sometimes this is visible through ps command line (feature of runsvdir) and
+# sometimes through Docker logs.
 # This script tries to find such programs and restarts their containers.
 
 for PID in $(ps -o pid,cmd -A | grep 'svlogd: pausing' | awk '{print $1}') ; do
@@ -14,4 +16,16 @@ for PID in $(ps -o pid,cmd -A | grep 'svlogd: pausing' | awk '{print $1}') ; do
   fi
 
   docker restart "$CONTAINER"
+done
+
+for CONTAINER in $(docker ps -q) ; do
+  if docker logs "$CONTAINER" 2>&1 | grep -q 'svlogd: pausing' ; then
+    # We first stop it, so that there is nothing going into logs anymore.
+    docker stop "$CONTAINER"
+
+    # Logs are retained between restarts, so we have to clear them first to not get into a loop.
+    truncate -s 0 "$(docker inspect --format='{{.LogPath}}' "$CONTAINER")"
+
+    docker restart "$CONTAINER"
+  fi
 done
